3e793fba2f5eaedc85d1b251d9d772350b046f63
Ahmed Al Haddad <ahmed.hdd@gmail.com>
2018-11-15 06:01:32 +0300

    Add links in context menu

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Menu, shell, systemPreferences, app } = require('electron')
+const { Menu, shell, systemPreferences, app, dialog } = require('electron')
 
 // require('electron-debug')();
 
@@ -17,39 +17,53 @@ var mb = menubar({
   preloadWindow: true,
   width: 280,
   height: 360,
-    resizable: false,
-    // alwaysOnTop: true,
+  resizable: false,
+  // alwaysOnTop: true,
 })
 
 const reportIssue = () => {
   shell.openExternal('https://github.com/cdes/figments-injector/issues/new');
 }
 
+const goToWebsite = () => {
+  shell.openExternal('https://github.com/cdes/figments-injector/');
+}
+
+const about = () => {
+  dialog.showMessageBox({
+    type: 'info',
+    message: 'Created by Ahmed Al-Haddad @cdes',
+    buttons: ['Close']
+  });
+}
+
 let tray = null;
 
 mb.on('ready', function ready () {
   console.log('app is ready')
-
+  
   const contextMenu = Menu.buildFromTemplate([
-    {label: 'Version 1.0', type: 'normal', enabled: false},
+    {label: 'About', type: 'normal', click: about},
+    {label: 'Website', type: 'normal', click: goToWebsite},
     {label: 'Report Issues', type: 'normal', click: reportIssue},
     {type: 'separator'},
-    {label: 'Quit', type: 'normal', role: 'quit'}
+    {label: 'Quit', type: 'normal', role: 'quit'},
+    {label: 'Version 1.0', type: 'normal', enabled: false},
   ])
-
+  
   tray = this.tray;
-
+  
   tray.on('right-click', function () {
     mb.hideWindow();
     tray.popUpContextMenu(contextMenu);
   })
-
+  
   mb.showWindow();
-
+  
 })
 
 mb.on('after-create-window', () => {
-    // mb.window.openDevTools({mode: 'detach'});
+  // mb.window.openDevTools({mode: 'detach'});
 });
 
 mb.on('hide', () => {
